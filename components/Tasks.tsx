9e4f073dccdcdde114bcf960858cdd44b6c0ace9
omarghandour <alex.forget50@gmail.com>
2024-01-09 00:20:25 +0200

    first commit

--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -1,11 +1,53 @@
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const Tasks = ({ task }: any) => {
   return (
-    <div className=" w-2/5 bg-white">
-      {task?.map((task: any) => {
-        return <div key={task.id}>{task.title}</div>;
-      })}
+    <div>
+      <div className=" w-2/5">
+        {task?.map((task: any) => {
+          return <div key={task.id}>{task.title}</div>;
+        })}
+      </div>
+      <Table className=" w-3/4">
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+          {task?.map((task: any) => {
+            return (
+              <TableRow key={task.id}>
+                <TableCell className="font-medium w-1/2">
+                  {task.title}
+                </TableCell>
+                <TableCell>Paid</TableCell>
+                <TableCell>Credit Card</TableCell>
+                <TableCell className="text-right">$250.00</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 };
